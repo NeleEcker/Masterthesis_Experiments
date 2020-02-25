e73e761a8bc75255910eb334fe45837e80b63d5e
Nele Ecker <nele.ecker@hotmail.de>
2020-02-25 14:02:10 +0100

    remove comments

--- example_train_transe.py
+++ example_train_transe.py
@@ -7,12 +7,12 @@ os.environ['CUDA_VISIBLE_DEVICES']='7'
 #Input training files from benchmarks/FB15K/ folder.
 con = config.Config()
 #True: Input test files from the same folder.
-con.set_in_path("./masterthesis/nell/")
-#con.set_in_path("./masterthesis/DBPedia500KNew/")
+#con.set_in_path("./masterthesis/nell/")
+con.set_in_path("./masterthesis/DBPedia500KNew/")
 con.set_test_link_prediction(True)
 con.set_test_triple_classification(False)
 con.set_work_threads(8)
-con.set_train_times(2)
+con.set_train_times(1)
 con.set_nbatches(500)
 con.set_alpha(0.001)
 con.set_margin(1.0)
@@ -25,9 +25,9 @@ con.set_true_negative_triples(True)
 con.set_neg_sample_version(3)
 
 #Models will be exported via tf.Saver() automatically.
-con.set_export_files("./masterthesis/nellResults/TransE/trueFalseV3correct/model.vec.tf", 0)
+con.set_export_files("./masterthesis/dbpediaResultsSampleNew/experiment/model.vec.tf", 0)
 #Model parameters will be exported to json files automatically.
-con.set_out_files("./masterthesis/nellResults/TransE/trueFalseV3correct/embedding.vec.json")
+con.set_out_files("./masterthesis/dbpediaResultsSampleNew/experiment/embedding.vec.json")
 #Initialize experimental settings.
 con.init()
 print("I finished the initalization")
@@ -37,11 +37,4 @@ print("The model was set successfully")
 #Train the model.
 con.run()
 print("Finished Training")
-#To test models after training needs "set_test_flag(True)".
-#con.test()
-#print("Finished Testing")
-#con.predict_head_entity(152, 9, 5)
-#con.predict_tail_entity(151, 9, 5)
-#con.predict_relation(151, 152, 5)
-#con.predict_triple(151, 152, 9)
-#con.predict_triple(151, 152, 8)
+
